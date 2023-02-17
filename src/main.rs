0c45e17132a136221a56b4032d78f7352bc62fd2
boredcoder411 <105937655+boredcoder411@users.noreply.github.com>
2023-02-17 17:37:31 +0100

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    let request = String::from_utf8_lossy(&buffer[..]);
+    println!("{}", request);
+    let response = "HTTP/1.1 200 OK\r\n\r\nHello, World!";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    println!("Server listening on port 8080");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr().unwrap());
+                handle_client(stream);
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
+}
