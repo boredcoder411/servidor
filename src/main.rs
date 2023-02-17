322d2bd76ac6ebd4560dff1ae245e5bd9f5f6976
boredcoder411 <taamarcos@gmail.com>
2023-02-17 19:09:56 +0100

    this bish buggy af

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,36 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
+use std::fs;
 
 fn handle_client(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
     let request = String::from_utf8_lossy(&buffer[..]);
+
     println!("{}", request);
-    let response = "HTTP/1.1 200 OK\r\n\r\nHello, World!";
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+
+    if request.contains("GET / HTTP/1.1") {
+        let content = fs::read_to_string("index.html").unwrap();
+
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            content.len(),
+            content
+        );
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let response = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
     println!("Server listening on port 8080");
+
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
